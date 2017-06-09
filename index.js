d700a2b4cc58f065b7b6c5ff52bc138bd75c5615
Robin Vercammen <robinvercammen1993@hotmail.com>
2017-06-09 14:26:28 +0200

    add comments

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-var request=require("request"),PF=require("./pathsolver.js"),axios=require("axios")
-request("http://mazeretreat.azurewebsites.net/mazes/09a655dd-6cbd-4793-89cf-604d2b0a8330",function(e,t,a){var r=JSON.parse(a).split("\r\n"),n=[],o=[],s=r.map(function(e,t){return e.split("").map(function(e,a){return"#"===e?1:("S"===e&&(n=[a,t]),"F"===e&&(o=[a,t]),0)})})
-console.log(s)
-var i=new PF.Grid(s),u=new PF.AStarFinder,d=u.findPath(n[0],n[1],o[0],o[1],i),c=d.map(function(e){return e.join(",")}).join(";"),l={TeamName:"Robin en Yassine",MazeId:"09a655dd-6cbd-4793-89cf-604d2b0a8330",Solution:c}
-axios["default"].post("http://mazeretreat.azurewebsites.net/solutions/09a655dd-6cbd-4793-89cf-604d2b0a8330",l).then(console.log,console.log)})
+var request = require("request"), PF = require("./pathsolver.js"), axios = require("axios")
+request("http://mazeretreat.azurewebsites.net/mazes/09a655dd-6cbd-4793-89cf-604d2b0a8330", function (e, t, a) {
+    // split input to array and replace # with 0 and so on
+    var r = JSON.parse(a).split("\r\n"), n = [], o = [], s = r.map(function (e, t) { return e.split("").map(function (e, a) { return "#" === e ? 1 : ("S" === e && (n = [a, t]), "F" === e && (o = [a, t]), 0) }) })
+    console.log(s)
+    // s is grid in form [[],[],[]]
+    var i = new PF.Grid(s), u = new PF.AStarFinder, d = u.findPath(n[0], n[1], o[0], o[1], i), c = d.map(function (e) { return e.join(",") }).join(";"), l = { TeamName: "Robin en Yassine", MazeId: "09a655dd-6cbd-4793-89cf-604d2b0a8330", Solution: c }
+    axios["default"].post("http://mazeretreat.azurewebsites.net/solutions/09a655dd-6cbd-4793-89cf-604d2b0a8330", l).then(console.log, console.log)
+})
